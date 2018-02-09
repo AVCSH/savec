bc64895bf1a4cbb6562b087c16f091ee92bde08e
SUK <inasuk@gmail.com>
2018-02-09 23:57:30 +0100

    Our lovely Event entity has a title right now

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -10,20 +10,49 @@ use Ramsey\Uuid\UuidInterface;
  */
 class Event
 {
-    /**
-     * @var UuidInterface
-     *
-     * @ORM\Id
-     * @ORM\Column(type="uuid", unique=true)
-     */
-    protected $id;
+	/**
+	 * @var UuidInterface
+	 *
+	 * @ORM\Id
+	 * @ORM\Column(type="uuid", unique=true)
+	 */
+	protected $id;
 
 
-    /** 
-     * ItemType constructor
-     *
-     */
-    public function __construct() {
-	    $this->id = Uuid::uuid4();
-    }
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=256)
+	 */
+	protected $title;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string")
+	 */
+	protected $description;
+
+
+	/** 
+	 * ItemType constructor
+	 *
+	 */
+	public function __construct() {
+		$this->id = Uuid::uuid4();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTitle(): string {
+		return $this->title;
+	}
+
+	/**
+	 * @param string $title
+	 */
+	public function setTitle(string $title): void {
+		$this->title = $title;
+	}
 }
