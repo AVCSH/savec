276da9ca9e862d739b0280f16336928709a7bc73
Michal Hošna <mh@michalhosna.com>
2018-03-01 14:48:00 +0100

    Basic prooph

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,8 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Prooph\Bundle\EventStore\ProophEventStoreBundle::class => ['all' => true],
+    Prooph\Bundle\ServiceBus\ProophServiceBusBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
