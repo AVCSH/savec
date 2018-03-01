24e2cb1d63705faf4f9d50956d5fb758eb8ec467
Michal Hošna <mh@michalhosna.com>
2018-03-01 12:18:00 +0100

    Remove doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -3,9 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
 ];
